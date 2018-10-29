f3de0f7a7e869aed4428a2aa494ad8759ec873c8
Alexandr Karachev <a.karachev@centrvd.ru>
2018-10-29 15:19:06 +0300

    корректировки отображения слайда

--- frontend/views/story/view.php
+++ frontend/views/story/view.php
@@ -7,7 +7,7 @@ use frontend\widgets\RevealWidget;
 
 ?>
 <div class="row" style="height: 100%">
-	<div class="col-xs-12" style="height: 100%">
+	<div class="col-xs-12" style="height: 90%">
 		<h1><?= $model->title ?></h1>
 		<?= RevealWidget::widget(['data' => $model->body]) ?>
 	</div>
--- frontend/widgets/RevealWidget.php
+++ frontend/widgets/RevealWidget.php
@@ -22,32 +22,32 @@ class RevealWidget extends Widget
         $view = $this->getView();
         $asset = RevealAsset::register($view);
         $view->registerCssFile('/css/offline-v2.css');
-        $js = "
-        	Reveal.initialize({
-        		width: 1024,
-        		height: 576,
-        		margin: 0.05,
-        		controls: true,
-        		progress: true,
-        		history: true,
-        		mouseWheel: false,
-        		showNotes: true,
-        		slideNumber: true,
-        		autoSlide: 0 || 0,
-        		autoSlideStoppable: true,
-        		center: false,
-        		shuffle: false,
-        		loop: false,
-        		rtl: false
-		       // dependencies: [
-		         //   { src: 'js/reveal-plugins/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		         //   { src: 'js/reveal-plugins/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-		         //   { src: 'js/reveal-plugins/highlight/highlight.js', async: true, callback: function() { hljs.initHighlighting(); hljs.initHighlightingOnLoad(); } },
-		        //    { src: 'js/reveal-plugins/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
-		        //    { src: 'js/reveal-plugins/zoom/zoom.js', async: true }
-		        //]
-    });
-        ";
+        $js = <<< JS
+Reveal.initialize({
+	width: 1024,
+	height: 576,
+	margin: 0.05,
+	controls: true,
+	progress: true,
+	history: false,
+	mouseWheel: false,
+	showNotes: true,
+	slideNumber: false,
+	autoSlide: 0 || 0,
+	autoSlideStoppable: true,
+	center: false,
+	shuffle: false,
+	loop: false,
+	rtl: false
+    //dependencies: [
+    //    { src: 'js/reveal-plugins/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    //    { src: 'js/reveal-plugins/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+    //    { src: 'js/reveal-plugins/highlight/highlight.js', async: true, callback: function() { hljs.initHighlighting(); hljs.initHighlightingOnLoad(); } },
+    //    { src: 'js/reveal-plugins/notes/notes.js', async: true, condition: function() { return !!document.body.classList; } },
+    //    { src: 'js/reveal-plugins/zoom/zoom.js', async: true }
+    //]
+});
+JS;
         $view->registerJs($js);
     }
 
