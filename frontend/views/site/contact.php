a3ca63cff0b81a355b6f2902844da8bf5074fe72
xarrper <xarrper@gmail.com>
2018-11-18 12:59:08 +0400

    Контакты и сброс пароля

--- frontend/views/site/contact.php
+++ frontend/views/site/contact.php
@@ -11,25 +11,34 @@ use yii\captcha\Captcha;
 $this->title = 'Контакты';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-contact">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>
-        Если у вас есть деловые вопросы или другие вопросы, пожалуйста, заполните следующую форму, чтобы связаться с нами. Спасибо.
-    </p>
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'contact-form']); ?>
-                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
-                <?= $form->field($model, 'email') ?>
-                <?= $form->field($model, 'subject') ?>
-                <?= $form->field($model, 'body')->textarea(['rows' => 6]) ?>
-                <?= $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-6">{input}</div></div>',
-                ]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary', 'name' => 'contact-button']) ?>
+<div class="bg-grey form-container">
+    <div class="container">
+        <div class="site-contact">
+            <div class="info-text">
+                Если у вас есть деловые вопросы или другие вопросы, пожалуйста, заполните следующую форму, чтобы связаться с нами. Спасибо.
+            </div>
+            <div class="row widget-search">
+                <div class="col-lg-5">
+                    <?php $form = ActiveForm::begin(['id' => 'contact-form',
+                        'fieldConfig' => [
+                            'template' => '<div class="el-container">{label}{input}{error}</div>',
+                            'labelOptions' => ['class' => 'widget-title'],
+                            'inputOptions' => ['class'=> null]
+                        ]
+                    ]); ?>
+                        <?= $form->field($model, 'name', ['inputOptions' => ['placeholder' => 'Имя пользователя']])->textInput(['autofocus' => true]) ?>
+                        <?= $form->field($model, 'email', ['inputOptions' => ['placeholder' => 'Email пользователя']]) ?>
+                        <?= $form->field($model, 'subject', ['inputOptions' => ['placeholder' => 'Тема']]) ?>
+                        <?= $form->field($model, 'body', ['inputOptions' => ['placeholder' => 'Сообщение']])->textarea(['rows' => 6]) ?>
+                        <?= $form->field($model, 'verifyCode', ['inputOptions' => ['class'=> null, 'placeholder' => 'Код подтверждения']])->widget(Captcha::className(), [
+                            'template' => '<div class="row"><div class="col-lg-3">{image}</div><div class="col-lg-9">{input}</div></div>',
+                            'options' => ['class'=> null, 'placeholder' => 'Код подтверждения'],
+                        ]) ?>                        
+                        <?= $form->field($model, 'email', ['inputOptions' => ['placeholder' => 'Email пользователя']])->textInput(['autofocus' => true]) ?>    
+                        <?= Html::submitButton('Отправить', ['class' => 'custom-btn white form-btn']) ?>    
+                    <?php ActiveForm::end(); ?>
                 </div>
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -6,21 +6,30 @@
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use common\widgets\Alert;
 
 $this->title = 'Запросить сброс пароля';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-request-password-reset">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>Пожалуйста, заполните свой адрес электронной почты. Будет отправлена ссылка на сброс пароля.</p>
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form']); ?>
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+<div class="bg-grey form-container">
+    <div class="container">
+        <?= Alert::widget() ?>
+        <div class="site-request-password-reset">
+        <div class="info-text">Пожалуйста, заполните свой адрес электронной почты. Будет отправлена ссылка на сброс пароля.</div>
+            <div class="row widget-search">
+                <div class="col-lg-5">
+                    <?php $form = ActiveForm::begin(['id' => 'request-password-reset-form',
+                        'fieldConfig' => [
+                            'template' => '<div class="el-container">{label}{input}{error}</div>',
+                            'labelOptions' => ['class' => 'widget-title'],
+                            'inputOptions' => ['class'=> null]
+                        ]
+                    ]); ?>                        
+                        <?= $form->field($model, 'email', ['inputOptions' => ['placeholder' => 'Email пользователя']])->textInput(['autofocus' => true]) ?>    
+                        <?= Html::submitButton('Отправить', ['class' => 'custom-btn white form-btn']) ?>    
+                    <?php ActiveForm::end(); ?>
                 </div>
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
