f12376f73df087325070a9b5f7729845b910bd19
Alexandr Karachev <alex.kara4ev@gmail.com>
2019-03-16 13:26:32 +0300

    #WKS-40 возможность загрузки изображений для слайдов в редакторе

---
+++ backend/services/StoryEditorService.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace backend\services;
+
+use yii;
+use yii\web\UploadedFile;
+use common\models\Story;
+use common\services\StoryService;
+use backend\components\StoryHtmlReader;
+use backend\components\StoryEditor;
+
+class StoryEditorService
+{
+
+	protected function uploadImage($form, $model)
+	{
+        $imageFile = UploadedFile::getInstance($form, 'image');
+        if ($imageFile) {
+            $storyService = new StoryService();
+            $storyImagesPath = $storyService->getImagesFolderPath($model);
+            $slideImageFileName = Yii::$app->security->generateRandomString() . '.' . $imageFile->extension;
+            $imagePath = "{$storyImagesPath}/$slideImageFileName";
+            if ($imageFile->saveAs($imagePath)) {
+            	return $storyService->getImagesFolderPath($model, true) . '/' . $slideImageFileName;
+        	}
+        }
+        return '';
+	}
+
+	public function updateSlide($form)
+	{
+        $storyModel = Story::findOne($form->story_id);
+        $imagePath = $this->uploadImage($form, $storyModel);
+
+        $reader = new StoryHtmlReader();
+        $story = $reader->loadStoryFromHtml($storyModel->body);
+
+        $editor = new StoryEditor($story);
+        $editor->updateSlide($form->slide_index, $form->text, $imagePath);
+
+        //$editor->setSlideText($editorModel->slide_index, $editorModel->text);
+        $body = '<div class="slides">' . $editor->getStoryMarkup() . '</div>';
+        $storyModel->saveBody($body);
+        //$success = true;
+	}
+
+}
