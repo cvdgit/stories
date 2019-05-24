dc5af1677d1987db4358de789654e105810b37c3
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-05-24 21:17:29 +0300

    команда для удаления лишних файлов #WKS-50

--- console/controllers/StoryController.php
+++ console/controllers/StoryController.php
@@ -2,10 +2,11 @@
 
 namespace console\controllers;
 
-use Yii;
 use yii\console\Controller;
 use common\models\Story;
 use common\components\StoryCover;
+use Yii;
+use yii\db\Query;
 
 class StoryController extends Controller
 {
@@ -29,5 +30,34 @@ class StoryController extends Controller
 		}
 		$this->stdout('Done!' . PHP_EOL);
 	}
+
+	public function actionClearStoryFiles()
+    {
+
+        $path = Yii::getAlias('@public/slides_file/');
+        $existFiles = [];
+        if ($handle = opendir($path)) {
+            while (false !== ($entry = readdir($handle))) {
+                if ($entry !== '.' && $entry !== '..') {
+                    $existFiles[] = $entry;
+                }
+            }
+            closedir($handle);
+        }
+
+        $query = (new Query())
+            ->from('{{%story}}')
+            ->where('story_file IS NOT NULL')
+            ->andWhere('source_id = 2');
+        $storyFiles = [];
+        foreach ($query->each() as $story) {
+            $storyFiles[] = $story['story_file'];
+        }
+
+        $files = array_diff($existFiles, $storyFiles);
+        foreach ($files as $fileName) {
+            $this->stdout($fileName . PHP_EOL);
+        }
+    }
 
 }
