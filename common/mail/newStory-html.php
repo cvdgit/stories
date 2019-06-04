85e725d63bbb619e93155233539c56d9262b828b
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-06-04 11:46:55 +0300

    UniSender рассылка о новых историях #WKS-56

--- common/mail/newStory-html.php
+++ common/mail/newStory-html.php
@@ -8,12 +8,12 @@ use yii\helpers\Html;
 ?>
 <div>
     <p>Здравствуйте, {{Name}}</p>
-    <p>На Wikids добавлена новая история - <?= Html::a($story->title, Yii::$app->urlManagerFrontend->createAbsoluteUrl(['/story/view', 'alias' => $story->alias]), 'https') ?></p>
+    <p>На Wikids добавлена новая история - <?= Html::a($story->title, Yii::$app->urlManagerFrontend->createAbsoluteUrl(['/story/view', 'alias' => $story->alias])) ?></p>
     <table>
         <tbody>
             <tr>
                 <td>
-                    <?= Html::img(StoryCover::getListThumbPath($story->cover)) ?>
+                    <?= Html::img(Yii::$app->urlManagerFrontend->baseUrl . StoryCover::getListThumbPath($story->cover)) ?>
                 </td>
                 <td>
                     <?= $story->description ?>
