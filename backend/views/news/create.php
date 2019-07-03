45b0d2d93bdbf49d9a2ae6f95fe5d38540eceb5d
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-07-03 11:48:56 +0300

    Блог #WKS-58

--- backend/views/news/create.php
+++ backend/views/news/create.php
@@ -1,6 +1,8 @@
 <?php
 
+use vova07\imperavi\Widget;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
 /**
@@ -10,26 +12,29 @@ use yii\widgets\ActiveForm;
  */
 
 ?>
-
-<?php $this->beginBlock('header'); ?>
-<div class="header-title">
-    <div class="container">
-        <h4>Suggest news</h4>
-    </div>
-</div>
-<?php $this->endBlock(); ?>
-
 <div class="news-add">
-
     <?php $form = ActiveForm::begin(['id' => 'news-add']) ?>
-
     <?= $form->field($model, 'title')->textInput(['maxlength' => 250]) ?>
-
-    <?= $form->field($model, 'text')->textarea() ?>
-
+    <?= $form->field($model, 'text')->widget(Widget::class, [
+        'settings' => [
+            'lang' => 'ru',
+            'minHeight' => 200,
+            'imageUpload' => Url::to(['/news/image-upload']),
+            'plugins' => [
+                'clips',
+                'fullscreen',
+                'imagemanager',
+            ],
+            'clips' => [
+                ['Lorem ipsum...', 'Lorem...'],
+                ['red', '<span class="label-red">red</span>'],
+                ['green', '<span class="label-green">green</span>'],
+                ['blue', '<span class="label-blue">blue</span>'],
+            ],
+        ],
+    ]) ?>
     <div class="form-group">
-        <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        <?= Html::submitButton('Создать запись', ['class' => 'btn btn-primary']) ?>
     </div>
     <?php ActiveForm::end() ?>
-
 </div>
