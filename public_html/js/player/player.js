66d70f83ccf71a527ad43df6e7fa619c39e8da72
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-07-12 20:58:28 +0300

    story player fullscreen on landscape orientation

--- public_html/js/player/player.js
+++ public_html/js/player/player.js
@@ -44,7 +44,7 @@ var WikidsStoryFeedback = (function() {
 })();
 
 var WikidsPlayer = (function(document, $) {
-"use strict"
+
     var $playerContainer = $('.story-container');
 
     function enterFullscreen()
