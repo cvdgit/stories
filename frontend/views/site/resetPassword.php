ae8ee95b711c518c89750004e2a11f79719e30b1
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-07-23 12:23:03 +0300

    fix ошибки при восстановлении пароля

--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
-/* @var $model \frontend\models\ResetPasswordForm */
+/* @var $model frontend\models\ResetPasswordForm */
 
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
@@ -12,19 +12,23 @@ $this->setMetaTags($title,
                    $title,
                    '',
                    $title);
-$this->params['breadcrumbs'][] = $title;
 ?>
-<div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
-    <p>Выберите новый пароль:</p>
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-                <div class="form-group">
-                    <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
-                </div>
-            <?php ActiveForm::end(); ?>
+<div class="container">
+    <h1 class="text-center"><?= Html::encode($this->title) ?></h1>
+    <div class="site-reset-password">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <p class="info-text">Выберите новый пароль:</p>
+                <?php $form = ActiveForm::begin([
+                        'id' => 'reset-password-form',
+                    'options' => [
+                        'class' => 'story-form',
+                    ],
+                ]); ?>
+                <?= $form->field($model, 'password', ['inputOptions' => ['placeholder' => 'Новый пароль']])->label(false) ?>
+                        <?= Html::submitButton('Сохранить', ['class' => 'btn']) ?>
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
