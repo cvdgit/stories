3c4f7c6aafb1cf9deaf59f87e87d40de2a8fd5f3
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-08-28 19:17:01 +0300

    возможность удалять текстовые блоки в редакторе

--- backend/views/editor/_text_form.php
+++ backend/views/editor/_text_form.php
@@ -23,5 +23,6 @@ echo $form->field($model, 'text_size')->textInput();
 echo $form->field($model, 'text')->textArea(['rows' => 4]);
 echo $form->field($model, 'slide_id')->hiddenInput()->label(false);
 echo $form->field($model, 'block_id')->hiddenInput()->label(false);
-echo Html::submitButton('Сохранить', ['class' => 'btn btn-primary']);
+echo Html::submitButton('Сохранить', ['class' => 'btn btn-primary', 'style' => 'margin-right: 20px']);
+echo Html::a('Удалить блок', '#', ['class' => 'btn btn-danger', 'onclick' => "StoryEditor.deleteBlock('" . $model->block_id . "')"]);
 ActiveForm::end();
