ecf3a24e03d49233e533dfaa097afdfaaec6c944
alex.kara4ev@gmail.com <g00d_1uck!@#>
2019-11-14 15:20:51 +0300

    Плейлисты

--- frontend/widgets/views/playlists.php
+++ frontend/widgets/views/playlists.php
@@ -1,19 +1,29 @@
 <?php
 
+use yii\helpers\Html;
 use yii\helpers\Url;
 
 /** @var $models common\models\Playlist[] */
 ?>
-<div class="row">
+<div class="flex-row row story-list">
     <?php foreach ($models as $model): ?>
     <?php $story = $model->stories[0]; ?>
     <div class="col-xs-offset-1 col-xs-10 col-sm-offset-0 col-sm-6 col-md-offset-0 col-md-6 col-lg-offset-0 col-lg-3">
-        <div class="category-item">
+        <div class="story-item">
             <a href="<?= Url::toRoute(['/story/view', 'alias' => $story->alias, 'list' => $model->id]) ?>">
-                <div class="category-item-image-wrapper">
+                <div class="story-item-image">
+                    <div class="story-item-image-overlay">
+                        <span></span>
+                    </div>
                     <img src="<?= $story->getBaseModel()->getCoverRelativePath() ?>" alt="">
                 </div>
-                <h3><?= $model->title ?></h3>
+                <div class="story-item-caption">
+                    <p class="flex-text"></p>
+                    <p>
+                        <h3 class="story-item-name"><?= Html::encode($model->title) ?></h3>
+                        <span class="story-item-category"> Историй: <?= count($model->stories) ?></span>
+                    </p>
+                </div>
             </a>
         </div>
     </div>
