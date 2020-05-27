48add06b1bf2ff483ebcc81300092930e46a2f88
Karachev Alexander <alex.kara4ev@gmail.com>
2020-05-27 17:14:41 +0300

    Wikids API

--- api/modules/v1/controllers/StoryController.php
+++ api/modules/v1/controllers/StoryController.php
@@ -4,11 +4,20 @@
 namespace api\modules\v1\controllers;
 
 
+use api\modules\v1\models\Story;
+use yii\data\ActiveDataProvider;
 use yii\rest\ActiveController;
 
 class StoryController extends ActiveController
 {
 
     public $modelClass = 'api\modules\v1\models\Story';
+
+    public function actionIndex()
+    {
+        return new ActiveDataProvider([
+            'query' => Story::find(),
+        ]);
+    }
 
 }
