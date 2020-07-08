bca08dd13f086c4ced24bbaeaddb4fc323f90e24
Karachev Alexandr <alex.kara4ev@gmail.com>
2020-07-08 10:00:37 +0300

    Кеширование изображений в историях

--- frontend/controllers/ImageController.php
+++ frontend/controllers/ImageController.php
@@ -30,8 +30,14 @@ class ImageController extends Controller
         $response = Yii::$app->response;
         $response->format = Response::FORMAT_RAW;
         $response->headers->add('content-type', 'image/jpeg');
+
+        $imagePath = Yii::getAlias('@public/admin/upload/') . $image->folder . '/' . $image->hash . '.jpeg';
+        $response->headers->add('Cache-control: max-age=', (60*60*24*365));
+        $response->headers->add('Expires:', gmdate(DATE_RFC1123,time()+60*60*24*365));
+        $response->headers->add('Last-Modified:', gmdate(DATE_RFC1123, filemtime($imagePath)));
+
         try {
-            $img_data = file_get_contents(Yii::getAlias('@public/admin/upload/') . $image->folder . '/' . $image->hash . '.jpeg');
+            $img_data = file_get_contents($imagePath);
         }
         catch (\Exception $ex) {
             throw new HttpException(404);
