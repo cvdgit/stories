435c7ac5747c54b5a337984dc9f21e1b2ab2fd29
Karachev Alexandr <alex.kara4ev@gmail.com>
2020-10-27 17:43:55 +0300

    Возможность указывать строго соответствие для правильных ответов

---
+++ console/migrations/m201027_125700_add_strict_answer_column_to_story_test_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles adding columns to table `{{%story_test}}`.
+ */
+class m201027_125700_add_strict_answer_column_to_story_test_table extends Migration
+{
+
+    private $tableName = '{{%story_test}}';
+    private $tableColumnName = 'strict_answer';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addColumn($this->tableName, $this->tableColumnName, $this->tinyInteger()->notNull()->defaultValue(0));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropColumn($this->tableName, $this->tableColumnName);
+    }
+
+}
