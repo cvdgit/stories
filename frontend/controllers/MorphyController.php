85ce25eed7b9fcb1a96e06018fb7dd6abb398ddf
Karachev Alexandr <alex.kara4ev@gmail.com>
2021-03-29 11:58:14 +0300

    phpmorphy

--- frontend/controllers/MorphyController.php
+++ frontend/controllers/MorphyController.php
@@ -26,25 +26,68 @@ class MorphyController extends Controller
         ];
     }
 
+    private function getBaseWord(Morphy $morphy, string $word): string
+    {
+        $matchPseudoRoot = $this->getPseudoRoot($morphy, $word);
+        if (!empty($matchPseudoRoot)) {
+            return $matchPseudoRoot;
+        }
+        return $this->getBaseForm($morphy, $word);
+    }
+
+    private function getPseudoRoot(Morphy $morphy, string $word): ?string
+    {
+        return $morphy->getPseudoRoot(mb_strtoupper($word))[0];
+    }
+
+    private function getBaseForm(Morphy $morphy, string $word): ?string
+    {
+        return $morphy->getBaseForm(mb_strtoupper($word))[0];
+    }
+
+    private function getBaseArray(Morphy $morphy, array $words)
+    {
+        return array_map(function($item) use ($morphy) {
+            return $this->getBaseWord($morphy, $item);
+        }, $words);
+    }
+
     public function actionRoot()
     {
         Yii::$app->response->format = Response::FORMAT_JSON;
         $match = Yii::$app->request->post('match');
         $result = Yii::$app->request->post('result');
 
+        $matchArray = explode(' ', $match);
+        $resultArray = explode(' ', $result);
+
+        /** TODO: переписать на свою обертку для phpMorphy */
         $morphy = new Morphy();
 
-        $matchPseudoRoot = $morphy->getPseudoRoot(mb_strtoupper($match))[0];
-        $resultPseudoRoot = $morphy->getPseudoRoot(mb_strtoupper($result))[0];
-
-        $matchBaseForm = $morphy->getBaseForm(mb_strtoupper($match))[0];
-        $resultBaseForm = $morphy->getBaseForm(mb_strtoupper($result))[0];
-
-        if ((!empty($matchPseudoRoot) && !empty($resultPseudoRoot)) && ($matchPseudoRoot === $resultPseudoRoot)) {
-            $result = $match;
+        $matchPseudoRoot = '';
+        $resultPseudoRoot = '';
+        if (count($matchArray) === 1) {
+            $matchPseudoRoot = $this->getPseudoRoot($morphy, $match);
+            $resultPseudoRoot = $this->getPseudoRoot($morphy, $result);
+            $matchBaseForm = $this->getBaseForm($morphy, $match);
+            $resultBaseForm = $this->getBaseForm($morphy, $result);
+            if ((!empty($matchPseudoRoot) && !empty($resultPseudoRoot)) && ($matchPseudoRoot === $resultPseudoRoot)) {
+                $result = $match;
+            }
+            else {
+                if ((!empty($matchBaseForm) && !empty($resultBaseForm)) && $matchBaseForm === $resultBaseForm) {
+                    $result = $match;
+                }
+            }
         }
         else {
-            if ((!empty($matchBaseForm) && !empty($resultBaseForm)) && $matchBaseForm === $resultBaseForm) {
+            $baseMatchArray = $this->getBaseArray($morphy, $matchArray);
+            $baseResultArray = $this->getBaseArray($morphy, $resultArray);
+            $baseMatch = implode(' ', $baseMatchArray);
+            $baseResult = implode(' ', $baseResultArray);
+            $matchBaseForm = $baseMatch;
+            $resultBaseForm = $baseResult;
+            if (strcasecmp($baseMatch, $baseResult) === 0) {
                 $result = $match;
             }
         }
