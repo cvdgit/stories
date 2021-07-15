9e5c2181951e65f3ed63b630cb44e4dddbf2490f
Karachev Alexandr <alex.kara4ev@gmail.com>
2021-07-15 13:21:39 +0300

    Новый редактор - Менеджер изображений - Возможность загрузить изображения в менеджер

--- console/migrations/m210715_095627_create_insert_trigger_for_image_slide_block_table.php
+++ console/migrations/m210715_095627_create_insert_trigger_for_image_slide_block_table.php
@@ -14,8 +14,14 @@ class m210715_095627_create_insert_trigger_for_image_slide_block_table extends M
     {
         $trigger = <<<SQL
 CREATE TRIGGER `insert_in_story_story_slide_image` AFTER INSERT ON `image_slide_block` 
-    FOR EACH ROW 
-    INSERT INTO story_story_slide_image SET story_slide_image_id = NEW.image_id, story_id = (SELECT story_id FROM story_slide WHERE id = NEW.slide_id)
+FOR EACH ROW
+BEGIN
+    DECLARE storyID integer;
+    SET @storyID := (SELECT story_id FROM story_slide WHERE id = NEW.slide_id);
+    IF NOT EXISTS(SELECT 1 FROM story_story_slide_image WHERE story_id = @storyID AND story_slide_image_id = NEW.image_id) THEN
+        INSERT INTO story_story_slide_image SET story_slide_image_id = NEW.image_id, story_id = @storyID;
+    END IF;
+END
 SQL;
         $command = Yii::$app->db->createCommand($trigger);
         $command->execute();
