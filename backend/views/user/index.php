5a0afb68206e54c730226b9c1278708464f87e4d
Karachev Alexandr <alex.kara4ev@gmail.com>
2021-09-17 15:42:09 +0300

    регистрация и авторизация без username

--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -1,15 +1,12 @@
 <?php
-
 use yii\grid\ActionColumn;
 use yii\helpers\Html;
 use yii\grid\GridView;
 use common\helpers\UserHelper;
 use backend\widgets\grid\RoleColumn;
-
 /* @var $this yii\web\View */
 /* @var $searchModel backend\models\UserSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
-
 $this->title = 'Пользователи';
 ?>
 <h1 class="page-header"><?= Html::encode($this->title) ?></h1>
@@ -22,7 +19,6 @@ $this->title = 'Пользователи';
     'options' => ['class' => 'table-responsive'],
     'columns' => [
         'id',
-        'username',
         'email:email',
         'last_activity:datetime',
         'created_at:datetime',
