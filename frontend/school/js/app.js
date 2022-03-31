4a7a9c45438a49bd358197064bad3602cdd21eb3
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-03-31 12:15:30 +0300

    school login modal fix

--- frontend/school/js/app.js
+++ frontend/school/js/app.js
@@ -3,6 +3,7 @@ import "bootstrap/js/dist/collapse";
 import "bootstrap/js/dist/dropdown";
 import "bootstrap/js/dist/tab";
 import "bootstrap/js/dist/modal";
+import "bootstrap/js/dist/button";
 import $ from 'jquery';
 
 $(".categories li").click(function(e) {
