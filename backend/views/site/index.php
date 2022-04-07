ab8376c1b50d5d39e6f75bc596cc7b37c9834e22
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-04-07 19:14:23 +0300

    админка главная страница - просмотры историй за сегодня

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -13,7 +13,7 @@ $this->title = 'Панель управления';
 <div class="site-index">
     <div class="body-content">
         <div class="row">
-            <div class="col-xs-3">
+            <div class="col-lg-3">
                 <h4>Сегодня</h4>
                 <ul class="list-group">
                     <li class="list-group-item"><span class="badge"><?= SummaryHelper::activatedSubscriptions() ?></span> Активировано подписок</li>
@@ -26,7 +26,7 @@ $this->title = 'Панель управления';
                     <?= Html::a('<span class="badge">' . SummaryHelper::activePayments() . '</span> Активных подписок', ['payment/index', 'status' => Payment::STATUS_VALID], ['class' => 'list-group-item list-group-item-info']) ?>
                 </ul>
             </div>
-            <div class="col-xs-9">
+            <div class="col-lg-9">
                 <div style="height: 400px">
                     <?= ChartJs::widget([
                         'type' => 'line',
@@ -68,9 +68,9 @@ $this->title = 'Панель управления';
         </div>
         <div class="row">
             <div class="col-lg-6">
-                <div class="table-responsive">
+                <div class="table-responsive" style="max-height: 40vh">
                     <table class="table table-condensed table-striped">
-                        <caption>Просмотренные сегодня истории</caption>
+                        <caption>Просмотренные сегодня истории (<?= count($todayStories) ?>)</caption>
                         <thead>
                         <tr>
                             <th>История</th>
