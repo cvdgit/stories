54c39dddf94798ecc4d27da6864353b69df2ee6f
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-06-21 12:14:18 +0300

    Доработки раздела с комментариями

--- backend/views/comment/index.php
+++ backend/views/comment/index.php
@@ -1,27 +1,48 @@
 <?php
-
+use yii\data\ActiveDataProvider;
+use yii\grid\ActionColumn;
 use yii\grid\GridView;
 use yii\helpers\Html;
-
-/* @var $this yii\web\View */
-/* @var $dataProvider yii\data\ActiveDataProvider */
-
+use yii\web\View;
+/* @var View $this */
+/* @var ActiveDataProvider $dataProvider */
 $this->title = 'Комментарии';
+$this->registerCss(<<<CSS
+.grid-row {
+    word-break: break-word;
+}
+CSS
+);
 ?>
 <h1 class="page-header"><?= Html::encode($this->title) ?></h1>
 <?= GridView::widget([
     'dataProvider' => $dataProvider,
+    'options' => ['class' => 'table-responsive'],
+    'rowOptions' => [
+        'class' => 'grid-row',
+    ],
     'columns' => [
         'id',
-        'story.title',
-        'user.username',
-        'body',
-        'created_at:datetime',
-        'updated_at:datetime',
         [
-            'class' => 'yii\grid\ActionColumn',
+            'attribute' => 'story.title',
+            'format' => 'raw',
+            'value' => static function($model) {
+                return Html::a($model->story->title, Yii::$app->urlManagerFrontend->createAbsoluteUrl(['story/view', 'alias' => $model->story->alias]), ['target' => '_blank']);
+            }
+        ],
+        [
+            'label' => 'Пользователь',
+            'attribute' => 'user.profileName',
+        ],
+        [
+            'enableSorting' => false,
+            'attribute' => 'body',
+            'filter' => 'ntext',
+        ],
+        'created_at:datetime',
+        [
+            'class' => ActionColumn::class,
             'template' => '{delete}',
         ],
     ],
 ]) ?>
-
