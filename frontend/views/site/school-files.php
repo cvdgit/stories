39ed9305cb4b261f09d7aa96aa85f8397d678541
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-07-25 17:29:56 +0300

    Страница со списком документов для школы

--- frontend/views/site/school-files.php
+++ frontend/views/site/school-files.php
@@ -4,7 +4,7 @@
 use modules\files\models\StudyFile;
 use yii\helpers\Html;
 use yii\helpers\Url;
-$title = 'Перечень документов';
+$title = 'Список документов для зачисления';
 $this->setMetaTags($title,
     $title,
     '',
@@ -13,20 +13,15 @@ $this->registerLinkTag(['rel' => 'canonical', 'href' => Url::canonical()]);
 ?>
 <div class="container">
     <main style="margin-bottom:40px">
-        <section style="padding:0">
+        <section style="padding:0;margin-bottom:30px">
             <h1 style="font-size:36px"><?= Html::encode($title) ?></h1>
         </section>
-        <section style="padding:0">
-            <ul class="list-unstyled">
-                <li style="margin: 16px 0">
-                    <a href="<?= StudyFile::getFileUrlByAlias('school_request') ?>">Заявление</a>
-                </li>
-                <li style="margin: 16px 0">
-                    <a href="<?= StudyFile::getFileUrlByAlias('school_agree') ?>">Согласие на обработку персональных данных</a>
-                </li>
-                <li style="margin: 16px 0">
-                    <a href="<?= StudyFile::getFileUrlByAlias('school_doc_list') ?>">Список документов для зачисления</a>
-                </li>
+        <section style="padding:0;line-height:1.8">
+            <ul>
+                <li style="margin-bottom:20px">Заявление на имя директора ОАНО СОШ «Пенаты» от родителя/законного представителя о зачислении ребёнка в качестве экстерна для прохождения промежуточной аттестации за курс соответствующего класса (скан-копия). <a style="color:#007bff" href="<?= StudyFile::getFileUrlByAlias('school_request') ?>">Бланк прилагается</a></li>
+                <li style="margin-bottom:20px">Свидетельство о рождении (скан-копия).</li>
+                <li style="margin-bottom:20px">Согласие на обработку персональных данных (скан-копия). <a style="color:#007bff" href="<?= StudyFile::getFileUrlByAlias('school_agree') ?>">Бланк прилагается</a></li>
+                <li style="margin-bottom:20px">Личное дело ученика, которое при переходе на семейный формат образования необходимо забрать из школы. Личное дело предоставляется для обучающихся 2-4 классов; для обучающихся 1-го класса личное дело будет заведено ОАНО СОШ «Пенаты». (Перед зачислением направляете нам скан-копию личного дела. Оригинал направляется по почте и хранится в ОАНО СОШ «Пенаты».)</li>
             </ul>
         </section>
     </main>
