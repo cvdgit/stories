f4c8382e722760d2b6347d5076ae414f6dc69e38
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-08-08 09:19:53 +0300

    edu

--- modules/edu/models/EduProgramSearch.php
+++ modules/edu/models/EduProgramSearch.php
@@ -14,7 +14,7 @@ class EduProgramSearch extends EduProgram
     /**
      * {@inheritdoc}
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             [['id'], 'integer'],
