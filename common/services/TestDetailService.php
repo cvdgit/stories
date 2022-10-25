577dd2b835277d7c9f4d5244198185b19f1e766e
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-10-25 12:04:45 +0300

    Статистика по обучению

--- common/services/TestDetailService.php
+++ common/services/TestDetailService.php
@@ -21,7 +21,7 @@ class TestDetailService
             ])
             ->from(['t' => StoryTest::tableName()])
             ->innerJoin(['question_history' => UserQuestionHistory::tableName()], 'question_history.test_id = t.id')
-            ->innerJoin(['answers' => UserQuestionAnswer::tableName()], 'question_history.id = answers.question_history_id')
+            ->leftJoin(['answers' => UserQuestionAnswer::tableName()], 'question_history.id = answers.question_history_id')
             ->where(['t.id' => $testId])
             ->andWhere(['question_history.student_id' => $studentId])
             ->orderBy(['question_history.created_at' => SORT_ASC])
