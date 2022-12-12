7d157cfc3b87e6c02b3e2589a3045c59b1f82133
Karachev Alexandr <alex.kara4ev@gmail.com>
2022-12-12 14:23:16 +0300

    story guest view

--- backend/components/book/views/slide.php
+++ backend/components/book/views/slide.php
@@ -31,7 +31,7 @@ MobileTestAsset::register($this);
         <?php else: ?>
             <?php if ($manager->texts->isEmpty() && !$manager->images->isEmpty()): ?>
                 <div class="row">
-                    <div class="col-lg-10 offset-lg-1">
+                    <div class="col-lg-10 col-lg-offset-1">
                         <?= BlockRenderer::renderImages($manager->images) ?>
                     </div>
                 </div>
