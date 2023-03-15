07b69695f0be64cf86bf5b359162449d06bed2f9
Alexandr Karachev <a.karachev@alial.group>
2023-03-15 09:31:55 +0300

    repetition view css fix

--- modules/edu/views/student/_repetition_item.php
+++ modules/edu/views/student/_repetition_item.php
@@ -16,7 +16,7 @@ use yii\helpers\Url;
         <div class="panel-img"></div>
         <div class="panel-inner">
             <div class="panel-header">
-                <span class="panel-header__text"><?= $model['header']; ?></span>
+                <span title="<?= $model['header']; ?>" class="panel-header__text" style="text-overflow: ellipsis; overflow: hidden; display: -webkit-box; -webkit-line-break: 2; -webkit-box-orient: vertical"><?= $model['header']; ?></span>
             </div>
         </div>
         <div class="panel-progress">
