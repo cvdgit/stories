dab2a0ea7f22efbe158ff42f29f1771d51033fb7
Alexandr Karachev <a.karachev@alial.group>
2023-09-14 17:29:15 +0300

    вес для вопросов

--- console/controllers/QuestionController.php
+++ console/controllers/QuestionController.php
@@ -5,13 +5,22 @@ namespace console\controllers;
 use backend\models\question\QuestionType;
 use common\models\StoryTestQuestion;
 use yii\console\Controller;
+use yii\db\Expression;
 
 class QuestionController extends Controller
 {
     public function actionPassTestWeight(): void
     {
-        $models = StoryTestQuestion::findAll(['type' => QuestionType::PASS_TEST]);
+        $models = StoryTestQuestion::find()
+          ->where([
+              'type' => QuestionType::PASS_TEST
+          ])
+        ->andWhere(['not', ['regions' => null]])
+        ->all();
         echo count($models) . PHP_EOL;
+        /*foreach ($models as $model) {
+
+        }*/
         echo "Done!" . PHP_EOL;
     }
 }
