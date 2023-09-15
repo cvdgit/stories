2956042ef972a29bf784c4478af85a44d65e73a6
Alexandr Karachev <a.karachev@alial.group>
2023-09-15 10:27:38 +0300

    вес для вопросов

--- console/controllers/QuestionController.php
+++ console/controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace console\controllers;
 
 use backend\models\question\QuestionType;
+use backend\services\PassTestService;
 use common\models\StoryTestQuestion;
 use yii\console\Controller;
 use yii\db\Expression;
@@ -10,6 +11,17 @@ use yii\helpers\Json;
 
 class QuestionController extends Controller
 {
+    /**
+     * @var PassTestService
+     */
+    private $passTestService;
+
+    public function __construct($id, $module, PassTestService $passTestService, $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->passTestService = $passTestService;
+    }
+
     public function actionPassTestWeight(): void
     {
         $models = StoryTestQuestion::find()
@@ -21,9 +33,11 @@ class QuestionController extends Controller
 
         $i = 0;
         foreach ($models as $model) {
+            /** @var StoryTestQuestion $model */
             $json = Json::decode($model->regions);
             if (isset($json['fragments']) && count($json['fragments']) > 1) {
-                $i++;
+                $model->weight = $this->passTestService->calcWeight($json);
+                $model->save();
             }
         }
         echo $i . PHP_EOL;
