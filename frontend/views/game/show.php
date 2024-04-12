b03263807981e29903c5ab2a39a6c63de4866a06
Alexandr Karachev <a.karachev@alial.group>
2024-04-12 14:32:09 +0300

    game test

--- frontend/views/game/show.php
+++ frontend/views/game/show.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use frontend\assets\GameAsset;
+use yii\helpers\Json;
 use yii\web\View;
 
 /**
@@ -11,8 +12,29 @@ use yii\web\View;
 
 GameAsset::register($this);
 
+/*$data = Json::encode([
+    "id":
+]);*/
+
+$this->registerJs(<<<JS
+window.gameData = {
+}
+JS
+);
 $this->registerJs($this->renderFile("@frontend/views/game/show.js"));
 ?>
 <div id="unity-container" class="unity-desktop">
     <canvas id="unity-canvas" width=1280 height=720 tabindex="-1"></canvas>
+    <div id="unity-loading-bar">
+        <div id="unity-logo"></div>
+        <div id="unity-progress-bar-empty">
+            <div id="unity-progress-bar-full"></div>
+        </div>
+    </div>
+    <div id="unity-warning"> </div>
+    <div id="unity-footer">
+        <div id="unity-webgl-logo"></div>
+        <div id="unity-fullscreen-button"></div>
+        <div id="unity-build-title">WikidsGame</div>
+    </div>
 </div>
