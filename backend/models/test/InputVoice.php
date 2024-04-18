19c4e95ca99784b34ae134a5b1cf61d92b9826b8
Alexandr Karachev <a.karachev@alial.group>
2024-04-18 15:43:05 +0300

    fix speech langs

--- backend/models/test/InputVoice.php
+++ backend/models/test/InputVoice.php
@@ -9,10 +9,12 @@ class InputVoice
     {
         return [
             'Google русский' => 'Google русский (ru-RU)',
-            'Microsoft Irina Desktop - Russian' => 'Microsoft Irina Desktop - Russian (ru-RU)',
+            'Microsoft Irina - Russian (Russia)' => 'Microsoft Irina Desktop - Russian (ru-RU)',
             'Google US English' => 'Google US English (en-US)',
-            'Microsoft David Desktop - English (United States)' => 'Microsoft David Desktop - English (United States) (en-US)',
-            'Microsoft Zira Desktop - English (United States)' => 'Microsoft Zira Desktop - English (United States) (en-US)',
+            'Google UK English Female' => 'Google UK English Female',
+            'Google UK English Male' => 'Google UK English Male',
+            'Microsoft David - English (United States)' => 'Microsoft David Desktop - English (United States) (en-US)',
+            'Microsoft Zira - English (United States)' => 'Microsoft Zira Desktop - English (United States) (en-US)',
         ];
     }
 
