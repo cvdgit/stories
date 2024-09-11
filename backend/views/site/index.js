15788e28cfdcd8dba4c35c5fce2103faedf1de09
Alexandr Karachev <a.karachev@alial.group>
2024-09-11 08:45:40 +0300

    changelog

--- backend/views/site/index.js
+++ backend/views/site/index.js
@@ -1,5 +1,9 @@
 (function() {
   const $modal = $('#changelog-modal')
+  $modal.on('hide.bs.modal', function() {
+    $(this).removeData('bs.modal');
+    $(this).find('.modal-content').html('');
+  });
   $('#changelogs').on('click', '.changelog-item', e => {
     e.preventDefault()
     const url = $(e.target).closest('a.changelog-item').attr('href')
