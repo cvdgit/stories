1c651e633362f333c49f94bc282e0023b1d361d8
Alexandr Karachev <a.karachev@alial.group>
2024-09-20 09:49:23 +0300

    admin changelog access

--- backend/modules/changelog/Module.php
+++ backend/modules/changelog/Module.php
@@ -19,7 +19,7 @@ class Module extends \yii\base\Module
                 'rules' => [
                     [
                         'allow' => true,
-                        'roles' => [UserRoles::ROLE_ADMIN],
+                        'roles' => [UserRoles::ROLE_TEACHER],
                     ],
                 ],
             ]
