2043518bec84f514befc28a8de79bb6364910a3f
Alexandr Karachev <a.karachev@alial.group>
2024-10-18 11:24:50 +0300

    getcourse

---
+++ frontend/controllers/GetcourseController.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\Request;
+
+class GetcourseController extends Controller
+{
+    public $enableCsrfValidation = false;
+
+    public function actionWebhook(Request $request): void
+    {
+        $rawBody = $request->rawBody;
+        file_put_contents(Yii::getAlias('@public/upload/' . time()), $rawBody);
+    }
+}
