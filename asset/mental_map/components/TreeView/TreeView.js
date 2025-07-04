b42bafac00ef57c280465fe466624f5e5fc4c515
Alexandr Karachev <a.karachev@alial.group>
2025-07-04 10:21:03 +0300

    ментальная карта в виде дерева - редактор - возможность копирования блоков

--- asset/mental_map/components/TreeView/TreeView.js
+++ asset/mental_map/components/TreeView/TreeView.js
@@ -1,7 +1,12 @@
 import React, {createContext, useEffect, useReducer, useRef, useState} from "react";
-import {changeNodeAtPath, SortableTree} from "@nosferatu500/react-sortable-tree";
+import {
+  addNodeUnderParent,
+  changeNodeAtPath,
+  getNodeAtPath,
+  insertNode, removeNodeAtPath,
+  SortableTree
+} from "@nosferatu500/react-sortable-tree";
 import "./TreeView.css";
-import {addNodeUnderParent, removeNodeAtPath} from "./tree";
 import api, {parseError} from "../../Api";
 import TreeReducer from "../../Lib/TreeReducer";
 import {v4 as uuidv4} from 'uuid'
@@ -133,6 +138,37 @@ export default function TreeView({texts}) {
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
                          stroke="currentColor" className="size-6">
                       <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15"/>
+                    </svg>
+                  </button>,
+                  <button
+                    style={{width: '30px', padding: '6px'}}
+                    onClick={() => {
+
+                      const sibling = getNodeAtPath({
+                        treeData: state.treeData,
+                        path,
+                        getNodeKey,
+                        ignoreCollapsed: true,
+                      })
+
+                      const treeData = insertNode({
+                        treeData: state.treeData,
+                        depth: path.length - 1,
+                        minimumTreeIndex: sibling.treeIndex + 1,
+                        newNode: {...node, id: uuidv4(), children: []},
+                        getNodeKey,
+                        ignoreCollapsed: false,
+                      }).treeData
+                      dispatch({
+                        type: 'update_tree',
+                        treeData,
+                      })
+                    }}
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
+                         stroke="currentColor" className="size-6">
+                      <path strokeLinecap="round" strokeLinejoin="round"
+                            d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"/>
                     </svg>
                   </button>,
                   <button
