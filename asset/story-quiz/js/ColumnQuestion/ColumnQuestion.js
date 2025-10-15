deb1b7d9e25979286893f6b15ba6d6630080eae4
Alexandr Karachev <a.karachev@alial.group>
2025-10-15 14:56:14 +0300

    Решение в столбик - умножение на одно число

--- asset/story-quiz/js/ColumnQuestion/ColumnQuestion.js
+++ asset/story-quiz/js/ColumnQuestion/ColumnQuestion.js
@@ -187,27 +187,53 @@ function multiplyTemplate(template, {steps, digits, firstDigit, secondDigit, sho
   const el = document.createElement('div')
   el.classList.add('template__calculation')
   showValue = showValue || false
-  steps.map((step, i) => {
+
+  if (steps.length === 1 && (firstDigit.length === 1 || secondDigit.length === 1)) {
+    template.querySelector('.secondDigitRow').classList.remove('calculationRow_last')
     const wrap = document.createElement('div')
-    if (i === steps.length - 1) {
-      wrap.appendChild(createHelpersRow(digits, i + 1))
-      wrap.appendChild(createDigitsRow(digits + 1, {offset: i, rowExtraClassName: 'calculationRow_last', data: {step: step.step}, value: showValue ? String(step.resultInt) : null}))
-      wrap.appendChild(createHelpersRow(firstDigit.length + secondDigit.length, 1))
-      wrap.appendChild(createResultsRow(firstDigit.length + secondDigit.length + 1, {value: result, showValue: Boolean(showValue)}))
-    } else {
-      wrap.appendChild(createHelpersRow(digits, i + 1))
-      wrap.appendChild(createDigitsRow(digits + 1, {offset: i, data: {step: step.step}, value: showValue ? String(step.resultInt) : null}))
-      if (i === 0) {
-        const symbolEl = document.createElement('div')
-        symbolEl.classList.add('template__symbol', 'template__symbol_plus'/*, 'template__symbol_plus-helper'*/)
-        symbolEl.innerHTML = `
+    wrap.classList.add('calculationRow_first')
+    wrap.appendChild(createHelpersRow(firstDigit.length + secondDigit.length, 1))
+    wrap.appendChild(createResultsRow(firstDigit.length + secondDigit.length + 1, {
+      value: result,
+      showValue: Boolean(showValue)
+    }))
+    el.appendChild(wrap)
+  } else {
+    steps.map((step, i) => {
+      const wrap = document.createElement('div')
+      if (i === steps.length - 1) {
+        wrap.appendChild(createHelpersRow(digits, i + 1))
+        wrap.appendChild(createDigitsRow(digits + 1, {
+          offset: i,
+          rowExtraClassName: 'calculationRow_last',
+          data: {step: step.step},
+          value: showValue ? String(step.resultInt) : null
+        }))
+        wrap.appendChild(createHelpersRow(firstDigit.length + secondDigit.length, 1))
+        wrap.appendChild(createResultsRow(firstDigit.length + secondDigit.length + 1, {
+          value: result,
+          showValue: Boolean(showValue)
+        }))
+      } else {
+        wrap.appendChild(createHelpersRow(digits, i + 1))
+        wrap.appendChild(createDigitsRow(digits + 1, {
+          offset: i,
+          data: {step: step.step},
+          value: showValue ? String(step.resultInt) : null
+        }))
+        if (i === 0) {
+          const symbolEl = document.createElement('div')
+          symbolEl.classList.add('template__symbol', 'template__symbol_plus'/*, 'template__symbol_plus-helper'*/)
+          symbolEl.innerHTML = `
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"><path fill-rule="evenodd" clip-rule="evenodd" d="M8.25 13.75C9.35457 13.75 10.25 14.6454 10.25 15.75V22C10.25 23.1046 11.1454 24 12.25 24C13.3546 24 14.25 23.1046 14.25 22V15.75C14.25 14.6454 15.1454 13.75 16.25 13.75H22C23.1046 13.75 24 12.8546 24 11.75C24 10.6454 23.1046 9.75 22 9.75H16.25C15.1454 9.75 14.25 8.85457 14.25 7.75V2C14.25 0.895431 13.3546 0 12.25 0C11.1454 0 10.25 0.89543 10.25 2V7.75C10.25 8.85457 9.35457 9.75 8.25 9.75H2C0.89543 9.75 0 10.6454 0 11.75C0 12.8546 0.89543 13.75 2 13.75H8.25Z" fill="#F09235"></path></svg>
           `
-        wrap.appendChild(symbolEl)
+          wrap.appendChild(symbolEl)
+        }
       }
-    }
-    el.appendChild(wrap)
-  })
+      el.appendChild(wrap)
+    })
+  }
+
   template.querySelector('.template').appendChild(el)
 }
 
