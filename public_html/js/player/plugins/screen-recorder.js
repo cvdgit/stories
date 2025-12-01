37dfe009b8c42eb8862f3e21d112ff98dc5c6897
Alexandr Karachev <a.karachev@centrvd.ru>
2025-12-01 16:40:46 +0300

    screen recording api

--- public_html/js/player/plugins/screen-recorder.js
+++ public_html/js/player/plugins/screen-recorder.js
@@ -38,6 +38,71 @@
     return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
   }
 
+  function InnerDialog(container, title, content) {
+    const html = `<div class="slide-hints-wrapper" style="background-color: white; position: absolute">
+    <div class="retelling-dialog-inner">
+        <div class="retelling-dialog-header">
+            <h2>${title}</h2>
+            <div class="header-actions">
+                <button type="button" class="hints-close">&times;</button>
+            </div>
+        </div>
+        <div id="dialog-body" class="retelling-dialog-body"></div>
+    </div>
+</div>
+`
+    this.showHandler = null
+    this.hideHandler = null
+
+    const hideDialog = () => {
+
+      Reveal.configure({keyboard: true})
+
+      if ($(container).find('.slide-hints-wrapper').length) {
+        $(container)
+          .find('.slide-hints-wrapper')
+          .hide()
+          .remove();
+      }
+      $('.reveal .story-controls').show()
+
+      if (typeof this.hideHandler === "function") {
+        this.hideHandler()
+      }
+    }
+
+    this.show = () => {
+
+      Reveal.configure({keyboard: false})
+
+      const $element = $(html)
+
+      $element.find("#dialog-body").append(content)
+      $element.on("click", ".hints-close", hideDialog)
+
+      $('.reveal .story-controls').hide()
+
+      container
+        .append($element)
+        .find(".slide-hints-wrapper")
+        .show();
+
+      if (typeof this.showHandler === "function") {
+        this.showHandler($element)
+      }
+    }
+
+    this.hide = hideDialog;
+
+    this.onShow = callback => {
+      this.showHandler = callback
+    }
+
+    this.onHide = callback => {
+      this.hideHandler = callback
+    }
+  }
+
   Reveal.addEventListener('ready', () => {
 
     const $overlay = $(
@@ -46,16 +111,22 @@
           <p class="screen-recording-error-wrap" style="font-size: 24px; line-height: 30px; color: #dc3545; display: none">
             <strong class="screen-recording-error"></strong>
           </p>
+          <div class="start-screen-recording-wrap" style="display: flex; flex-direction: row; gap: 10px; align-items: center">
           <button class="start-screen-recording btn" type="button">
             Начать запись экрана <img alt="..." style="width: 20px; display: none" src="/img/loading.gif" />
           </button>
+          <a class="show-screen-records" href="">Записи</a>
+</div>
+
           <div class="stop-screen-recording-wrap" style="display: none">
             <span>Идет запись... </span>
             <span class="screen-recorder-timer">00:00:00</span>
             <button class="stop-screen-recording btn btn-sm" type="button">
               Стоп <img alt="..." style="width: 20px; display: none" src="/img/loading.gif" />
             </button>
+            <a class="show-screen-records" href="">Записи</a>
           </div>
+
       </div>`
     )
 
@@ -63,6 +134,66 @@
     let mediaRecorder;
 
     const socket = io(config.ws_host);
+
+    async function fetchVideos({wsHost, storyId, userId}) {
+      const response = await fetch(`${wsHost}/getScreenVideos/${storyId}/${userId}`)
+      if (!response.ok) {
+        return [];
+      }
+      return await response.json();
+    }
+
+    async function renderVideoList(container) {
+
+      container
+        .find('.videos-container')
+        .html('<img style="width: 100px; height: 100px" src="/img/loading.gif" alt="">');
+
+      const params = {
+        wsHost: config.ws_host,
+        storyId: config.story_id,
+        userId: config.user_id
+      };
+      const files = await fetchVideos(params);
+      if (files.length === 0) {
+        container
+          .find('.videos-container')
+          .html('Нет записей экрана');
+        return;
+      }
+
+      container.find('.videos-container').empty();
+      container.find('.videos-container').append('<div class="sessions-wrap"></div>');
+
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      };
+      files.files.map(({session, videos}) => {
+        const $session = $(`<div><h4>Сессия</h4><div class="videos-wrap"></div></div>`);
+        videos.map(video => {
+          const formattedDateTime = new Intl.DateTimeFormat('ru-RU', options)
+            .format(new Date(video.time));
+          $session
+            .find('.videos-wrap')
+            .append(
+              `<div class="videos-video">
+<div style="position: relative"><video controls style="max-width: 100%">
+<source src="${config.ws_host}${video.url}" type="video/webm" />
+</video>
+${video.status === 'process' ? '<div class="videos-video-process"><img src="/img/loading.gif" alt="">В обработке...</div>' : ''}
+</div>
+<div style="font-size: 16px; padding: 4px 0">${formattedDateTime}</div>
+</div>`
+            );
+        })
+        container.find('.sessions-wrap').append($session);
+      });
+    }
 
     $overlay.find('.start-screen-recording').on('click', async ({target}) => {
 
@@ -169,6 +300,24 @@
 
       mediaRecorder.stop();
     });
+
+    $overlay.find('.show-screen-records').on('click', (e) => {
+      e.preventDefault();
+
+      const $content = $(`<div class="videos-container"></div>`);
+
+      const dialog = new InnerDialog($overlay, `Записи экрана (<a style="font-weight: 600" class="videos-load" href="">Обновить список</a>)`, $content);
+      dialog.onShow(async (container) => {
+
+        await renderVideoList(container);
+
+        container.find('.videos-load').on('click', async (e) => {
+          e.preventDefault();
+          await renderVideoList(container);
+        })
+      })
+      dialog.show();
+    })
 
     function stopRecording(payload) {
       socket.emit('screenData:end', payload);
--- ws/src/index.js
+++ ws/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
+import cors from 'cors'
 
-import { readFileSync } from "fs";
+import { readFileSync, readdirSync, statSync, createReadStream } from "fs";
+import path from 'node:path';
 import { createServer } from "https";
 import { Server } from "socket.io";
 
@@ -16,17 +18,121 @@ const app = express();
 const server = createServer(credentials, app);
 
 const io = new Server(server, {
- cors: {
-   origin: process.env.WIKIDS_ORIGIN
- }
+  cors: {
+    origin: process.env.WIKIDS_ORIGIN
+  }
 });
 
 io.on('connection', onConnection);
 
 server.listen(4000, () => {
- console.log('Server ready 🚀 ');
+  console.log('Server ready 🚀 ');
 });
 
-app.get('/getScreenVideos/:key', (req, res) => {
-  res.send(req.params);
+app.all('/', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
+var corsOptions = {
+  origin: process.env.WIKIDS_ORIGIN,
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.get('/getScreenVideos/:story/:user', cors(corsOptions), (req, res) => {
+  const { story, user } = req.params;
+  const storyId = Number(story);
+  const userId = Number(user);
+  const videoDirPath = `${import.meta.dirname}/video/story${storyId}/user${userId}`;
+  try {
+    const sessions = readdirSync(videoDirPath);
+    const files = [];
+    sessions
+      .sort((sessionNameA, sessionNameB) => {
+        const { mtime: timeA } = statSync(`${videoDirPath}/${sessionNameA}`);
+        const { mtime: timeB } = statSync(`${videoDirPath}/${sessionNameB}`);
+        return new Date(timeB).getTime() - new Date(timeA).getTime();
+      })
+      .map(sessionName => {
+
+        const videos = readdirSync(`${videoDirPath}/${sessionName}`);
+
+        const inProcessVideos = videos
+          .filter(v => v.startsWith('temp-'))
+          .map(v => v.replace('temp-', ''));
+
+        const { mtime: time } = statSync(`${videoDirPath}/${sessionName}`);
+
+        files.push({
+          session: sessionName,
+          time,
+          videos: videos.filter(v => !v.startsWith('temp-')).map(videoFileName => {
+            const id = videoFileName.replace('.webm', '');
+            const { mtime: time } = statSync(`${videoDirPath}/${sessionName}/${videoFileName}`);
+            if (inProcessVideos.includes(videoFileName)) {
+              return {
+                id,
+                status: 'process',
+                time,
+                url: `/screenVideo/${storyId}/${userId}/${sessionName.replace('session', '')}/${id}`
+              }
+            }
+            return {
+              id,
+              status: 'ok',
+              time,
+              url: `/screenVideo/${storyId}/${userId}/${sessionName.replace('session', '')}/${id}`
+            }
+          }),
+        })
+      });
+    res.send({
+      ok: true,
+      files
+    });
+  } catch (ex) {
+    res.send({ ok: false })
+  }
+});
+
+app.get('/screenVideo/:story/:user/:session/:video', cors(corsOptions), (req, res) => {
+
+  const { story, user, session: sessionId, video: videoId } = req.params;
+  const storyId = Number(story);
+  const userId = Number(user);
+
+  const filePath = `${import.meta.dirname}/video/story${storyId}/user${userId}/session${sessionId}/${videoId}.webm`;
+
+  try {
+    const stat = statSync(filePath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+    if (range) {
+      const parts = range.replace(/bytes=/, '').split('-');
+      const start = parseInt(parts[0], 10);
+      const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1;
+      const chunkSize = end - start + 1;
+      const file = createReadStream(filePath, { start, end });
+      const headers = {
+        'Content-Type': 'video/webm',
+        'Content-Length': chunkSize,
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+      };
+      res.status(206).set(headers);
+      file.pipe(res);
+    } else {
+      const headers = {
+        'Content-Type': 'video/webm',
+        'Content-Length': fileSize,
+        'Accept-Ranges': 'bytes',
+      };
+      res.status(200).set(headers);
+      createReadStream(filePath).pipe(res);
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal server error');
+  }
 });
