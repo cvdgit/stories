aa9316dc9349efbf00abd3f9b596b7efc0a35fa4
Alexandr Karachev <a.karachev@centrvd.ru>
2026-07-07 15:38:33 +0300

    Профиль - статистика. Ментальные карты. Учитывать параметр "все выделенные слова есть в ответе"

--- frontend/views/training/_detail_mental_map_content.php
+++ frontend/views/training/_detail_mental_map_content.php
@@ -53,6 +53,7 @@ CSS,
             <th class="header-col" style="width: 10%">% сходства</th>
             <th class="header-col" style="width: 10%">% закрытия текста</th>
             <th class="header-col" style="width: 10%">% закрытия важного текста</th>
+            <th class="header-col" style="width: 10%">Все важные слова в ответе</th>
         </tr>
         </thead>
         <tbody>
@@ -66,6 +67,7 @@ CSS,
                 <td class="data-col <?= MentalMap::fragmentIsDone((int) $imageData['overall_similarity'], (int) $imageData['threshold']) ? 'bg-success' : 'bg-danger' ?>"><?= $imageData['overall_similarity'] ?></td>
                 <td class="data-col"><?= $imageData['text_hiding_percentage'] ?></td>
                 <td class="data-col"><?= $imageData['text_target_percentage'] ?></td>
+                <td class="data-col"><?= (int) $imageData['all_important_words_included'] === 1 ? 'Да' : 'Нет' ?></td>
             </tr>
         <?php endforeach; ?>
         </tbody>
